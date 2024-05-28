a6fc9f83ed62aa8c06116f2371235e21c6d20c9f
Lucas Buzioli Fioravanti Palmieri <50854891+lbfpalmieri@users.noreply.github.com>
2024-05-28 20:31:42 -0300

    firstStep - startApp
    First app dev, css, js and more.

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render(){
+
+        /*Código jsx que cai ser transformado em java script puro*/
+        return(
+            <div className="calculator">
+
+            </div>
+        )
+    }
+}
