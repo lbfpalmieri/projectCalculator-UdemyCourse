df524d6ef447bf51febfa18a3700f49632d15e36
Lucas Buzioli Fioravanti Palmieri <50854891+lbfpalmieri@users.noreply.github.com>
2024-05-28 21:14:56 -0300

    firstStartAppWin10
    StartApp with win 10 ...

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,7 +4,7 @@ import './Calculator.css'
 export default class Calculator extends Component {
     render(){
 
-        /*Código jsx que cai ser transformado em java script puro*/
+        /*Código jsx que vai ser transformado em java script puro*/
         return(
             <div className="calculator">
 
